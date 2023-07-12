443139c3f682ae6ee1633cb817598e56bf160715
su mon hlaing <sumonhlaing1997@gmail.com>
2023-07-13 00:09:11 +0630

    response file done for get methods

---
+++ lib/model/get_one_post_response.dart
@@ -0,0 +1,24 @@
+class GetOnePostResponse {
+  int? id;
+  String? title;
+  String? body;
+  String? photo;
+
+  GetOnePostResponse({this.id, this.title, this.body, this.photo});
+
+  GetOnePostResponse.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    body = json['body'];
+    photo = json['photo'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['title'] = title;
+    data['body'] = body;
+    data['photo'] = photo;
+    return data;
+  }
+}
